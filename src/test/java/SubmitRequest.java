383735b89175dffa177c33f3383d548661d4147a
Vladimir Protcay <protcay2@mail.ru>
2022-04-25 16:48:11 +0300

    ispravil

--- src/test/java/SubmitRequest.java
+++ src/test/java/SubmitRequest.java
@@ -1,43 +1,38 @@
-import org.junit.jupiter.api.Test;
+
 import org.jetbrains.annotations.NotNull;
-import org.openqa.selenium.Keys;
+import org.junit.jupiter.api.Test;
 
 
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+
+
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 
 
+
 public class SubmitRequest {
-
-
     int delay = 15;
 
-    @NotNull
-    private String when(boolean trim) {
-        Calendar cl = new GregorianCalendar();
-        cl.add(Calendar.DATE, 7);
-        if (trim) {
-            return new SimpleDateFormat("d").format(cl.getTime());
-        } else {
-            return new SimpleDateFormat("dd.MM.yyyy").format(cl.getTime());
-        }
+@NotNull
+    public String generateDate(int days, String formatPattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(formatPattern));
     }
 
-        @Test
+    @Test
         void shouldSubmitRequest() {
             open("http://localhost:9999/");
             $("[data-test-id=city] .input__control").setValue("Москва");
-            $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false));
+            $("[data-test-id=date] [placeholder=\"Дата встречи\"]");
             $("[data-test-id=name] [name=name]").setValue("Джейсон Вурхез");
             $("[data-test-id=phone] [name=phone]").setValue("+79859292566");
             $("[data-test-id=agreement]>.checkbox__box").click();
             $("button>.button__content").click();
-            $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(delay)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + when(false)));
+            $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(delay)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " ));
         }
     }
